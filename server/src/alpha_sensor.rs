540f3bb2c10ee46261bf05535e17e59ccdce3f2d
Woland <me@woland.xyz>
2020-11-23 18:27:16 +0100

    main: extract alpha_sensor

---
+++ server/src/alpha_sensor.rs
@@ -0,0 +1,121 @@
+use btleplug::api::{Peripheral, UUID, Characteristic};
+use std::sync::mpsc;
+use std::time::Duration;
+
+pub struct AlphaSensor<P: Peripheral> {
+    pub peripheral: P,
+    characteristic: Characteristic,
+    data_receiver: mpsc::Receiver<Vec<u8>>
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum AlphaSensorPollError {
+    UnexpectedResponse,
+    SensorError,
+    Timeout,
+    SendFailed
+}
+
+pub struct AlphaSensorReading {
+    pub temperature: i8,
+    pub humidity: u8
+}
+
+impl<P: Peripheral> AlphaSensor<P> {
+    pub fn try_new(peripheral: P, characteristic: Characteristic) -> Option<Self> {
+
+        let (tx, rx) = mpsc::channel();
+        let notification_characteristic = characteristic.clone();
+        peripheral.on_notification(Box::new(move |notification| {
+            if notification.uuid == notification_characteristic.uuid {
+                tx.send(notification.value).expect("Send failure");
+            } else {
+                println!("Unexpected notification uuid: {}", notification.uuid);
+            }
+        }));
+
+
+        if Self::check_hello(&peripheral, &characteristic, &rx) {
+            Some(AlphaSensor {
+                peripheral, characteristic,
+                data_receiver: rx
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn inspect(peripheral: &P) -> Option<Characteristic> {
+        println!("Connecting to {} {}...", peripheral.address(), peripheral.properties().local_name.unwrap());
+
+        peripheral.connect()
+            .and_then(|_| {
+                println!("Discovering characteristics of {}...", peripheral.address());
+                peripheral.discover_characteristics()
+            })
+            .map_or_else(
+                |_| {
+                    println!("Disconnecting {}...", peripheral.address());
+                    if let Err(err) = peripheral.disconnect() {
+                        println!("Could not disconnect from device {}, {}", peripheral.address(), err);
+                    }
+                    None
+                },
+                |characteristics| {
+                    characteristics.iter()
+                        .find(|c| c.uuid == UUID::B16(0xFFE1))
+                        .map(|c| c.clone())
+                }
+            )
+    }
+
+    fn check_hello(peripheral: &P, characteristic: &Characteristic, rx: &mpsc::Receiver<Vec<u8>>) -> bool {
+        if let Err(_) = peripheral.command(characteristic, &[0x10u8]) {
+            return false;
+        }
+        match rx.recv_timeout(Duration::from_secs(5)) {
+            Ok(data) => {
+                if data.eq(&vec![0x00u8, 0xF0u8, 0x14u8, 0x4Du8]) {
+                    true
+                } else {
+                    println!("Hello data did not match!");
+                    false
+                }
+            }
+            Err(_) => {
+                println!("Hello timeout");
+                false
+            }
+        }
+    }
+
+    pub fn poll(&self) -> Result<AlphaSensorReading, AlphaSensorPollError> {
+        if let Err(_) = self.peripheral.command(&self.characteristic, &[0x66u8]) {
+            return Err(AlphaSensorPollError::SendFailed);
+        }
+        self.data_receiver
+            .recv_timeout(Duration::from_secs(5))
+            .map_or(Err(AlphaSensorPollError::Timeout), 
+                |data| {
+                    if data.len() == 4 {
+                        if data[0] == 0x00u8 {
+                            let temperature = i8::from_le_bytes([data[1]]);
+                            let humidity = data[2];
+                            Ok(AlphaSensorReading { temperature, humidity })
+                        } else {
+                            Err(AlphaSensorPollError::SensorError)
+                        }
+                    } else {
+                        Err(AlphaSensorPollError::UnexpectedResponse)
+                    }
+                }
+            )
+    }
+}
+
+impl<P: Peripheral> Drop for AlphaSensor<P> {
+    fn drop(&mut self) {
+        println!("Disconnecting dropped sensor {}...", self.peripheral.address());
+        let _ = self.peripheral.disconnect();
+    }
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,7 +2,7 @@ extern crate btleplug;
 #[macro_use] extern crate diesel;
 #[macro_use] extern crate diesel_migrations;
 
-use btleplug::api::{Central, CentralEvent, Peripheral, BDAddr, UUID, Characteristic, ValueNotification};
+use btleplug::api::{Central, CentralEvent, Peripheral, BDAddr};
 #[cfg(target_os = "linux")]
 use btleplug::bluez::{adapter::ConnectedAdapter, manager::Manager};
 #[cfg(target_os = "macos")]
@@ -27,15 +27,17 @@ use std::time::SystemTime;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Scope, rt::System};
 use std::vec::Vec;
 use std::sync::{mpsc, Arc, Mutex};
-use std::cell::RefCell;
 use std::sync::atomic::{Ordering, AtomicBool};
 use std::thread;
 use std::time::Duration;
 use chrono::prelude::*;
-use chrono::{NaiveDateTime};
+use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use diesel::prelude::*;
 use diesel::r2d2;
+
+mod alpha_sensor;
+use alpha_sensor::*;
 
 
 #[get("/")]
@@ -69,100 +71,6 @@ struct BleMaster<P: Peripheral> {
     to_inspect: Mutex<Vec<P>>,
     sensors: Mutex<Vec<AlphaSensor<P>>>
 
-}
-
-struct AlphaSensor<P: Peripheral> {
-    peripheral: P,
-    characteristic: Characteristic,
-    data_receiver: mpsc::Receiver<Vec<u8>>
-}
-
-#[derive(Debug, Eq, PartialEq)]
-enum AlphaSensorPollError {
-    UnexpectedResponse,
-    SensorError,
-    Timeout,
-    SendFailed
-}
-
-struct AlphaSensorReading {
-    pub temperature: i8,
-    pub humidity: u8
-}
-
-impl<P: Peripheral> AlphaSensor<P> {
-    fn try_new(peripheral: P, characteristic: Characteristic) -> Option<Self> {
-
-        let (tx, rx) = mpsc::channel();
-        let notification_characteristic = characteristic.clone();
-        peripheral.on_notification(Box::new(move |notification| {
-            if notification.uuid == notification_characteristic.uuid {
-                tx.send(notification.value).expect("Send failure");
-            } else {
-                println!("Unexpected notification uuid: {}", notification.uuid);
-            }
-        }));
-
-
-        if Self::check_hello(&peripheral, &characteristic, &rx) {
-            Some(AlphaSensor {
-                peripheral, characteristic,
-                data_receiver: rx
-            })
-        } else {
-            None
-        }
-    }
-
-    fn check_hello(peripheral: &P, characteristic: &Characteristic, rx: &mpsc::Receiver<Vec<u8>>) -> bool {
-        if let Err(_) = peripheral.command(characteristic, &[0x10u8]) {
-            return false;
-        }
-        match rx.recv_timeout(Duration::from_secs(5)) {
-            Ok(data) => {
-                if data.eq(&vec![0x00u8, 0xF0u8, 0x14u8, 0x4Du8]) {
-                    true
-                } else {
-                    println!("Hello data did not match!");
-                    false
-                }
-            }
-            Err(_) => {
-                println!("Hello timeout");
-                false
-            }
-        }
-    }
-
-    pub fn poll(&self) -> Result<AlphaSensorReading, AlphaSensorPollError> {
-        if let Err(_) = self.peripheral.command(&self.characteristic, &[0x66u8]) {
-            return Err(AlphaSensorPollError::SendFailed);
-        }
-        self.data_receiver
-            .recv_timeout(Duration::from_secs(5))
-            .map_or(Err(AlphaSensorPollError::Timeout), 
-                |data| {
-                    if data.len() == 4 {
-                        if data[0] == 0x00u8 {
-                            let temperature = i8::from_le_bytes([data[1]]);
-                            let humidity = data[2];
-                            Ok(AlphaSensorReading { temperature, humidity })
-                        } else {
-                            Err(AlphaSensorPollError::SensorError)
-                        }
-                    } else {
-                        Err(AlphaSensorPollError::UnexpectedResponse)
-                    }
-                }
-            )
-    }
-}
-
-impl<P: Peripheral> Drop for AlphaSensor<P> {
-    fn drop(&mut self) {
-        println!("Disconnecting dropped sensor {}...", self.peripheral.address());
-        let _ = self.peripheral.disconnect();
-    }
 }
 
 impl<P: Peripheral> BleMaster<P> {
@@ -203,30 +111,7 @@ impl<P: Peripheral> BleMaster<P> {
     }
 
     pub fn inspect(&self, peripheral: P) {
-
-        println!("Connecting to {} {}...", peripheral.address(), peripheral.properties().local_name.unwrap());
-
-        let characteristic = peripheral.connect()
-            .and_then(|_| {
-                println!("Discovering characteristics of {}...", peripheral.address());
-                peripheral.discover_characteristics()
-            })
-            .map_or_else(
-                |_| {
-                    println!("Disconnecting {}...", peripheral.address());
-                    if let Err(err) = peripheral.disconnect() {
-                        println!("Could not disconnect from device {}, {}", peripheral.address(), err);
-                    }
-                    None
-                },
-                |characteristics| {
-                    characteristics.iter()
-                        .find(|c| c.uuid == UUID::B16(0xFFE1))
-                        .map(|c| c.clone())
-                }
-            );
-
-        if let Some(characteristic) = characteristic {
+        if let Some(characteristic) = AlphaSensor::inspect(&peripheral) {
             println!("Found characteristics in {}", peripheral.address());
             let mut sensors = self.sensors.lock().expect("Poisoned mutex");
             if let Some(sensor) = AlphaSensor::try_new(peripheral.clone(), characteristic) {
